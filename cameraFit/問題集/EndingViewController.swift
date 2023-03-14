64587ca02069fa5fdffdfac6c3cd8ce8b1abbfeb
stopshien <stopforxcode@gmail.com>
2023-03-14 15:42:26 +0800

    2023/03/14 加入 AI 人聲，將最後結論唸出來

--- cameraFit/問題集/EndingViewController.swift
+++ cameraFit/問題集/EndingViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import AVFAudio
 
 class EndingViewController: UIViewController{
     
+    let synthesizer = AVSpeechSynthesizer()
+
     
     var loveText : String?
     var loveTextDP : String?
@@ -28,6 +31,13 @@ class EndingViewController: UIViewController{
         super.viewDidLoad()
               
         EndingTextView.text = "心中期盼理想的另一伴，會是像\(loveText ?? "")的角色，並且希望他是一個\(loveTextDP ?? "")的人。\n" + " 在別人的眼中，你的形象猶如\(selfText ?? "")般\(selfTextDP ?? "")。\n" + "你的內心較多時候偏向\(woodSeems ?? "")一點，\(woodSeems2 ?? "")。\n" + "與他人相處時大多也表現的\(landScapeMeanig ?? "")一點。"
+        
+        let utterance =  AVSpeechUtterance(string: EndingTextView.text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        utterance.volume = 0.5
+        utterance.rate = 0.1
+        utterance.pitchMultiplier = 0.5
+        synthesizer.speak(utterance)
       
 
     }
